163defb6e7432795e8e28df7323e032d91db14d0
eranfuld <eranfuld@gmail.com>
2019-01-15 17:00:26 +0200

    non-spot ec2, nginx, index.html, s3

---
+++ README.md
@@ -0,0 +1,5 @@
+1. start a server (spot) in AWS.-done
+2. The server should be a Linux server with NGINX (basic configuration), with simple index.html.-done
+3. the html should have your name The server will take the index.html from s3 and copy it in to the created server.
+4. Setup auto scaling group to this server with 40% CPU.-will require more time.
+5. ind a solution for monitoring this server/service-will require more time.
---
+++ Terraform/standardRequest.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+  access_key = "XXXXXXXXXXXXXXXXXX"
+  secret_key = "XXXXXXXXXXXXXXXXXXXXXXX"
+  region     = "eu-west-2"
+}
+
+resource "aws_key_pair" "my-key" {
+  key_name   = "my-key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+  # SSH access
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # HTTP access from the VPC
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-01419b804382064e4"
+  instance_type = "t2.micro"
+  key_name        = "${aws_key_pair.my-key.key_name}"
+  security_groups = ["${aws_security_group.allow_ssh.name}"]
+ provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y update",
+      "sudo yum -y install nginx",
+      "sudo service nginx start",
+      "sudo chmod 777 -R /usr/share/nginx/html/",
+      "aws configure set region eu-west-2 --profile myprofile",
+      "aws configure set aws_access_key_id XXXXXXXXXXXXXXXXXX --profile myprofile",
+      "aws configure set aws_secret_access_key XXXXXXXXXXXXXXXXXXXXXXX --profile myprofile",
+      "aws s3 cp s3://s3-35-177-250-45/index.html /usr/share/nginx/html/ --profile myprofile", 
+    ]
+
+connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = "${file("id_rsa")}"
+  }
+
+  }
+
+}
