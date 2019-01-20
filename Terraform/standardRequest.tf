1fa056254bd7d2994d069885b02f038790d72df0
eranfuld <eranfuld@gmail.com>
2019-01-20 12:17:55 +0200

    due to different user login (root instead of ec2-user), an absolute path is needed.

--- Terraform/standardRequest.tf
+++ Terraform/standardRequest.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 resource "aws_key_pair" "my-key" {
   key_name   = "my-key"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
+  public_key = "${file("/home/ec2-user/.ssh/id_rsa.pub")}"
 }
 
 resource "aws_security_group" "allow_ssh" {
